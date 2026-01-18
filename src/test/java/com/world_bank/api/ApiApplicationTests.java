7676ec4be6131329fa155aace58a5b0eb6966e1d
Umesh kad <umeshkad2025@gmail.com>
2026-01-18 20:58:30 +0530

    first commit

---
+++ src/test/java/com/world_bank/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.world_bank.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
